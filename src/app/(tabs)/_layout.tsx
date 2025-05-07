f7ff941214a1708e5086c34390bad01b328b7b2b
Ogoigbe1 <ogoigbe12@gmail.com>
2025-05-07 11:59:45 +0100

    `Removed all components and layouts from the app, including tabs, explore screen, home screen, not found screen, root layout, collapsible component, external link component, haptic tab component, hello wave component, parallax scroll view component, themed

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Tabs } from 'expo-router';
+import { Colors } from '@/src/constants/Colors';
+
+
+
+export default function TabLayout() {
+ 
+
+  return (
+    <Tabs>
+      
+    </Tabs>
+  );
+}
